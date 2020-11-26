30d0c24d4d4a8635bb6c851e2fc05b61f4284988
Joe Sweeney <joe@j1.io>
2020-11-26 13:13:57 -0500

    Remove some default things

--- lib/edgybot.ex
+++ lib/edgybot.ex
@@ -1,18 +1,2 @@
 defmodule Edgybot do
-  @moduledoc """
-  Documentation for `Edgybot`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Edgybot.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
