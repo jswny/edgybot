eade5d2dcf2827201fb4383d0d7b56339cf19e50
Joe Sweeney <joe@j1.io>
2020-11-27 02:39:14 -0500

    Start event consumers in main supervisor

--- lib/edgybot/application.ex
+++ lib/edgybot/application.ex
@@ -5,19 +5,24 @@ defmodule Edgybot.Application do
 
   use Application
   require Logger
+  alias Edgybot.Bot.EventConsumer
 
   @impl true
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: Edgybot.Worker.start_link(arg)
-      # {Edgybot.Worker, arg}
-    ]
-
     Logger.info("Starting...")
+
+    children = generate_event_consumer_children()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Edgybot.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp generate_event_consumer_children() do
+    # Generate one child ber thread
+    Enum.map(1..System.schedulers_online(), fn thread_number ->
+      {EventConsumer, [thread_number: thread_number]}
+    end)
   end
 end
