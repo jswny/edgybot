e481d9edbc9ddcb005aae507cb518e8107c7e7c1
Joe Sweeney <joe@j1.io>
2020-11-27 17:41:32 -0500

    Fix compiler paths for test

--- mix.exs
+++ mix.exs
@@ -6,11 +6,16 @@ defmodule Edgybot.MixProject do
       app: :edgybot,
       version: "0.1.0",
       elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
