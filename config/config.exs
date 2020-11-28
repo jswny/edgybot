7bde507d7ff140a8ccb76a6f72d3950648e7b714
Joe Sweeney <joe@j1.io>
2020-11-28 17:37:59 -0500

    Add ping command (#9)
    * Update logging levels for prod and test
    
    * Initial ping response

--- config/config.exs
+++ config/config.exs
@@ -6,6 +6,4 @@ config :edgybot,
 config :porcelain,
   goon_warn_if_missing: false
 
-if config_env() != :prod do
-  import_config "#{Mix.env()}.exs"
-end
+import_config "#{Mix.env()}.exs"
--- lib/edgybot/bot/event_consumer.ex
+++ lib/edgybot/bot/event_consumer.ex
@@ -21,6 +21,17 @@ defmodule Edgybot.Bot.EventConsumer do
   end
 
   @impl true
+  def handle_event({:MESSAGE_CREATE, message, _ws_state}) do
+    event_type = :MESSAGE_CREATE
+    Logger.debug("Received event: #{event_type}")
+
+    case message.content do
+      "/e ping" -> Api.create_message!(message.channel_id, "Pong!")
+      _ -> :ignore
+    end
+  end
+
+  @impl true
   def handle_event(event) do
     event_type = elem(event, 0)
     Logger.debug("Ignored event: #{event_type}")
