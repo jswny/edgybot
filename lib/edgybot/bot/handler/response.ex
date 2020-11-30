029b01729bc5cea749001b43a6ca5a6787e081bd
Joe Sweeney <joe@j1.io>
2020-11-29 19:25:54 -0500

    Add guards to response handler

--- lib/edgybot/bot/handler/response.ex
+++ lib/edgybot/bot/handler/response.ex
@@ -5,18 +5,20 @@ defmodule Edgybot.Bot.Handler.Response do
 
   def handle_response(:noop, _source), do: :noop
 
-  def handle_response({:error, reason}, %{channel_id: channel_id}) do
+  def handle_response({:error, reason}, %{channel_id: channel_id})
+      when is_binary(reason) and is_integer(channel_id) do
     reason = "`Error: #{reason}`"
     response = {:message, channel_id, reason}
     send_response(response)
   end
 
-  def handle_response({:message, content}, %{channel_id: channel_id}) do
+  def handle_response({:message, content}, %{channel_id: channel_id})
+      when is_binary(content) and is_integer(channel_id) do
     response = {:message, channel_id, content}
     send_response(response)
   end
 
-  defp send_response(response) do
+  defp send_response(response) when is_tuple(response) do
     case response do
       {:message, channel_id, content} -> Api.create_message!(channel_id, content)
     end
