d0f116daa64782631277d7f95cdab77f4f485bee
Joe Sweeney <joe@j1.io>
2020-11-29 19:17:09 -0500

    Add guards to event handler

--- lib/edgybot/bot/handler/event.ex
+++ lib/edgybot/bot/handler/event.ex
@@ -3,7 +3,7 @@ defmodule Edgybot.Bot.Handler.Event do
 
   alias Edgybot.Bot.Handler
 
-  def handle_event(event, payload) do
+  def handle_event(event, payload) when is_atom(event) do
     case event do
       :MESSAGE_CREATE ->
         message = payload
