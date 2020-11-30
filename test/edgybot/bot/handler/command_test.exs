5b06bd6e0094c2c920a0e8d8472c841ef34a11b8
Joe Sweeney <joe@j1.io>
2020-11-29 22:23:51 -0500

    Test errors for command handler

--- test/edgybot/bot/handler/command_test.exs
+++ test/edgybot/bot/handler/command_test.exs
@@ -3,21 +3,39 @@ defmodule Edgybot.Bot.Handler.CommandTest do
   alias Edgybot.Bot
   alias Edgybot.Bot.Handler.Command
 
+  describe "handle_command/2" do
+    test "with no command returns error" do
+      command_definitions = %{}
+      command = Bot.prefix()
+
+      assert {:error, "no command provided"} =
+               Command.handle_command(command, command_definitions)
+    end
+
+    test "with invalid command name returns error" do
+      command_definitions = %{}
+      command = build_command("foo")
+
+      assert {:error, "no matching command"} =
+               Command.handle_command(command, command_definitions)
+    end
+  end
+
   describe "is_command?/1" do
     test "with invalid content returns false" do
-      content = "foo"
-      result = Command.is_command?(content)
+      command = "foo"
+      result = Command.is_command?(command)
       assert !result
     end
 
-    test "with valid content returns true" do
-      content = build_command("foo")
-      result = Command.is_command?(content)
+    test "with valid command returns true" do
+      command = build_command("foo")
+      result = Command.is_command?(command)
       assert result
     end
   end
 
-  defp build_command(content) do
-    "#{Bot.prefix()} #{content}"
+  defp build_command(command) do
+    "#{Bot.prefix()} #{command}"
   end
 end
