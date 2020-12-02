83d1439546591e9efbb5ecfa57a0ca3c1ec8b47b
Joe Sweeney <joe@j1.io>
2020-12-02 00:53:14 -0500

    Fix Credo warnings

--- lib/edgybot/bot/handler/error.ex
+++ lib/edgybot/bot/handler/error.ex
@@ -2,11 +2,9 @@ defmodule Edgybot.Bot.Handler.Error do
   @moduledoc false
 
   def handle_error(fun) do
-    try do
-      fun.()
-    rescue
-      e ->
-        {:error, e.message, __STACKTRACE__}
-    end
+    fun.()
+  rescue
+    e ->
+      {:error, e.message, __STACKTRACE__}
   end
 end
