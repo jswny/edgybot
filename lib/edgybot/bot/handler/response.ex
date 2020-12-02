511819992c79b804f0a10e548e80da705cbfcc75
Joe Sweeney <joe@j1.io>
2020-12-01 22:24:30 -0500

    Send error responses as embeds

--- lib/edgybot/bot/handler/response.ex
+++ lib/edgybot/bot/handler/response.ex
@@ -2,14 +2,17 @@ defmodule Edgybot.Bot.Handler.Response do
   @moduledoc false
 
   alias Nostrum.Api
+  alias Nostrum.Struct.Embed
+
+  @color_red 16_734_003
 
   def handle_response(:noop, _source), do: :noop
 
-  def handle_response({:error, reason}, %{channel_id: channel_id})
+  def handle_response({:error, reason} = response, %{channel_id: channel_id})
       when is_binary(reason) and is_integer(channel_id) do
-    reason = "`Error: #{reason}`"
-    response = {:message, channel_id, reason}
-    send_response(response)
+    embed = build_error_embed(response)
+    new_response = {:message, channel_id, embed: embed}
+    send_response(new_response)
   end
 
   def handle_response({:message, content}, %{channel_id: channel_id})
@@ -20,7 +23,15 @@ defmodule Edgybot.Bot.Handler.Response do
 
   defp send_response(response) when is_tuple(response) do
     case response do
-      {:message, channel_id, content} -> Api.create_message!(channel_id, content)
+      {:message, channel_id, content_or_opts} ->
+        Api.create_message!(channel_id, content_or_opts)
     end
+  end
+
+  defp build_error_embed({:error, reason}) do
+    %Embed{}
+    |> Embed.put_title("Error")
+    |> Embed.put_color(@color_red)
+    |> Embed.put_field("Reason", reason)
   end
 end
