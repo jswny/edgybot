f92908e4e002587b016fd0e9113697f038e6f787
Joe Sweeney <joe@j1.io>
2020-12-04 02:58:00 -0500

    Test error censor logging

--- test/edgybot/bot/handler/error_test.exs
+++ test/edgybot/bot/handler/error_test.exs
@@ -1,5 +1,6 @@
 defmodule Edgybot.Bot.Handler.ErrorTest do
   use ExUnit.Case
+  import ExUnit.CaptureLog
   alias Edgybot.Bot.Handler.Error
 
   describe "handle_error/1" do
@@ -9,10 +10,12 @@ defmodule Edgybot.Bot.Handler.ErrorTest do
       assert {:error, "test", _} = Error.handle_error(fun, false)
     end
 
-    test "converts errors to tuples with internal error message and no stacktrace when censoring" do
+    test "converts errors to tuples to internal error message and no stacktrace and logs when censoring" do
       fun = fn -> raise "test" end
 
-      assert {:error, "internal error"} = Error.handle_error(fun, true)
+      assert capture_log(fn ->
+               assert {:error, "internal error"} = Error.handle_error(fun, true)
+             end) =~ "Erlang error: \"test\""
     end
 
     test "passes through non-error" do
