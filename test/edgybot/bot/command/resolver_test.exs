314be643c14c1cb5cb48dd820fa5b4b2dc66b6c7
Joe Sweeney <joe@j1.io>
2021-02-23 20:25:44 -0500

    Modify command resolver tests to test for parameters

--- test/edgybot/bot/command/resolver_test.exs
+++ test/edgybot/bot/command/resolver_test.exs
@@ -3,46 +3,54 @@ defmodule Edgybot.Bot.Command.ResolverTest do
   alias Edgybot.Bot.Command.Resolver
 
   describe "match_command/2" do
-    test "with command with no arguments returns matched command" do
+    test "with command with no arguments returns matched command and parameters" do
       parsed_command = [{:string, "foo"}]
       command_definitions = command_definitions_fixture()
 
       expected = "foo"
-      assert {:ok, ^expected} = Resolver.match_command(parsed_command, command_definitions)
+      assert {:ok, ^expected, []} = Resolver.match_command(parsed_command, command_definitions)
     end
 
-    test "with command with static string argument returns matched command" do
+    test "with command with static string argument returns matched command and parameters" do
       parsed_command = [{:string, "bar"}, {:string, "baz"}]
       command_definitions = command_definitions_fixture()
 
       expected = "bar"
-      assert {:ok, ^expected} = Resolver.match_command(parsed_command, command_definitions)
+
+      assert {:ok, ^expected, [{:string, "baz"}]} =
+               Resolver.match_command(parsed_command, command_definitions)
     end
 
-    test "with command with single string argument returns matched command" do
+    test "with command with single string argument returns matched command and parameters" do
       parsed_command = [{:string, "bar"}, {:string, "qux"}]
       command_definitions = command_definitions_fixture(%{"bar" => [:string]})
 
       expected = "bar"
-      assert {:ok, ^expected} = Resolver.match_command(parsed_command, command_definitions)
+
+      assert {:ok, ^expected, [{:string, "qux"}]} =
+               Resolver.match_command(parsed_command, command_definitions)
     end
 
-    test "with command with multiple string argument returns matched command" do
+    test "with command with multiple string argument returns matched command and parameters" do
       parsed_command = [{:string, "bar"}, {:string, "qux"}, {:string, "quux"}]
       command_definitions = command_definitions_fixture(%{"bar" => [:string]})
 
       expected = "bar"
-      assert {:ok, ^expected} = Resolver.match_command(parsed_command, command_definitions)
+
+      assert {:ok, ^expected, [{:string, "qux quux"}]} =
+               Resolver.match_command(parsed_command, command_definitions)
     end
 
-    test "with multiple arguments returns matched command" do
+    test "with multiple arguments returns matched command and parameters" do
       parsed_command = [{:string, "bar"}, {:string, "baz"}, {:string, "qux"}]
 
       command_definitions =
         command_definitions_fixture(%{"bar" => [:string, {:static_string, "qux"}]})
 
       expected = "bar"
-      assert {:ok, ^expected} = Resolver.match_command(parsed_command, command_definitions)
+
+      assert {:ok, ^expected, [{:string, "baz qux"}]} =
+               Resolver.match_command(parsed_command, command_definitions)
     end
   end
 
