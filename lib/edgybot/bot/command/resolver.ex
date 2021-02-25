cc6d0c3443782db7668bb157578545a097dc4ee8
Joe Sweeney <joe@j1.io>
2021-02-25 01:45:53 -0500

    Return resolved command arguments in resolver

--- lib/edgybot/bot/command/resolver.ex
+++ lib/edgybot/bot/command/resolver.ex
@@ -6,13 +6,18 @@ defmodule Edgybot.Bot.Command.Resolver do
       do: {:error, "no command provided"}
 
   def match_command(parsed_command, command_definitions) when is_list(parsed_command) do
-    command_list = Map.keys(command_definitions)
+    resolve_results =
+      command_definitions
+      |> Map.keys()
+      |> Enum.map(&compare_command(parsed_command, &1, command_definitions))
 
-    command_name =
-      Enum.find(command_list, &compare_command(parsed_command, &1, command_definitions))
+    resolve_result = Enum.find(resolve_results, &match?({:ok, _}, &1))
 
-    if command_name do
-      {:ok, command_name}
+    if resolve_result do
+      {:ok, resolved_command} = resolve_result
+      [{:string, resolved_command_name} | resolved_command_rest] = resolved_command
+
+      {:ok, resolved_command_name, resolved_command_rest}
     else
       {:error, "no matching command"}
     end
@@ -23,42 +28,72 @@ defmodule Edgybot.Bot.Command.Resolver do
     command_definition = command_definitions[command_name_to_match_against]
     command_definition = [{:static_string, command_name_to_match_against} | command_definition]
 
-    command_definition_count = Enum.count(command_definition)
-    parsed_command_count = Enum.count(parsed_command)
+    resolve_command_against_definition(parsed_command, command_definition, [])
+  end
 
-    count_match = parsed_command_count == command_definition_count
+  defp resolve_command_against_definition([], [], resolved_command)
+       when is_list(resolved_command) do
+    reversed_resolved_command = Enum.reverse(resolved_command)
+    {:ok, reversed_resolved_command}
+  end
 
-    if count_match do
-      compare_command_to_definition(parsed_command, command_definition)
+  defp resolve_command_against_definition([], command_definition, resolved_command)
+       when is_list(command_definition) and is_list(resolved_command) do
+    {:error, "unsatisfied command arguments", command_definition}
+  end
+
+  defp resolve_command_against_definition(parsed_command, [], resolved_command)
+       when is_list(parsed_command) and is_list(resolved_command) do
+    {:error, "extraneous command parameters", parsed_command}
+  end
+
+  defp resolve_command_against_definition(
+         [current_parsed_command_elem | parsed_command_rest],
+         [current_command_definition_elem | command_definition_rest],
+         resolved_command
+       )
+       when is_list(parsed_command_rest) and is_list(command_definition_rest) and
+              is_list(resolved_command) do
+    elems_match? =
+      match_command_elem_against_definition_elem(
+        current_parsed_command_elem,
+        current_command_definition_elem
+      )
+
+    if elems_match? do
+      new_resolved_command = [
+        current_parsed_command_elem | resolved_command
+      ]
+
+      resolve_command_against_definition(
+        parsed_command_rest,
+        command_definition_rest,
+        new_resolved_command
+      )
     else
-      false
+      {:error, "unsatisfied command definition element", current_parsed_command_elem,
+       current_command_definition_elem}
     end
   end
 
-  defp compare_command_to_definition(parsed_command, command_definition)
-       when is_list(parsed_command) and is_list(command_definition) do
-    parsed_command
-    |> Enum.zip(command_definition)
-    |> Enum.all?(fn {parsed_command_elem, command_definition_elem} ->
-      compare_command_elem_to_definition_elem(parsed_command_elem, command_definition_elem)
-    end)
-  end
-
-  defp compare_command_elem_to_definition_elem(
-         {:string, parsed_elem_string},
-         {:static_string, definition_elem_string}
+  defp match_command_elem_against_definition_elem(
+         {:string, parsed_command_elem},
+         {:static_string, definition_elem}
        )
-       when is_binary(parsed_elem_string) and is_binary(definition_elem_string) do
-    parsed_elem_string == definition_elem_string
+       when is_binary(parsed_command_elem) and is_binary(definition_elem) do
+    parsed_command_elem == definition_elem
   end
 
-  defp compare_command_elem_to_definition_elem(
-         {:string, _parsed_elem_string},
+  defp match_command_elem_against_definition_elem(
+         {:string, _parsed_command_elem},
          :string
        ) do
     true
   end
 
-  defp compare_command_elem_to_definition_elem(_parsed_command_elem, _command_definition_elem),
-    do: false
+  defp match_command_elem_against_definition_elem(
+         _parsed_command_elem,
+         _command_definition_elem
+       ),
+       do: false
 end
