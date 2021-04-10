dfc7c2e82b4594b6e945caec5160c32edf08a65c
Joe Sweeney <joe@j1.io>
2021-04-10 16:58:20 -0400

    Add more tests for trailing variable length string command args

--- test/edgybot/bot/command/resolver_test.exs
+++ test/edgybot/bot/command/resolver_test.exs
@@ -41,7 +41,7 @@ defmodule Edgybot.Bot.Command.ResolverTest do
                Resolver.resolve_command(parsed_command, command_definitions)
     end
 
-    test "with command with single string argument returns resolved command and parameters" do
+    test "with command with single trailing string argument returns resolved command and parameters" do
       parsed_command = [{:string, "bar"}, {:string, "qux"}]
       command_definitions = command_definitions_fixture(%{"bar" => [:string]})
 
@@ -52,12 +52,31 @@ defmodule Edgybot.Bot.Command.ResolverTest do
                Resolver.resolve_command(parsed_command, command_definitions)
     end
 
-    test "with command with multiple string arguments returns resolved command and parameters" do
+    test "with command with multiple trailing string arguments returns resolved command and parameters" do
       parsed_command = [{:string, "bar"}, {:string, "baz"}, {:string, "qux"}, {:string, "quux"}]
       command_definitions = command_definitions_fixture(%{"bar" => [:string]})
 
       expected_command_name = "bar"
       expected_command_args = [{:string, "baz qux quux"}]
+
+      assert {:ok, ^expected_command_name, ^expected_command_args} =
+               Resolver.resolve_command(parsed_command, command_definitions)
+    end
+
+    test "with command with arguments and multiple trailing string arguments returns resolved command and parameters" do
+      parsed_command = [
+        {:string, "bar"},
+        {:string, "baz"},
+        {:string, "qux"},
+        {:string, "quux"},
+        {:string, "quuz"}
+      ]
+
+      command_definitions =
+        command_definitions_fixture(%{"bar" => [{:static_string, "baz"}, :string]})
+
+      expected_command_name = "bar"
+      expected_command_args = [{:string, "baz"}, {:string, "qux quux quuz"}]
 
       assert {:ok, ^expected_command_name, ^expected_command_args} =
                Resolver.resolve_command(parsed_command, command_definitions)
