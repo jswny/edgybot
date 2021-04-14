681d05a315abbaac96ea8b188337773e3d4e1c02
Joe Sweeney <joe@j1.io>
2021-04-13 23:21:01 -0400

    Add member schema

---
+++ lib/edgybot/meta/member.ex
@@ -0,0 +1,23 @@
+defmodule Edgybot.Meta.Member do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Edgybot.Meta.{Guild, User}
+
+  schema "members" do
+    belongs_to(:guild, Guild)
+    belongs_to(:user, User)
+
+    timestamps()
+  end
+
+  def changeset(member, params \\ %{}) do
+    member
+    |> cast(params, [:guild_id, :user_id])
+    |> validate_required([:guild_id, :user_id])
+    |> assoc_constraint(:guild)
+    |> assoc_constraint(:user)
+    |> unique_constraint([:guild_id, :user_id])
+  end
+end
