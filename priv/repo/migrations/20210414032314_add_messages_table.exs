676f9b5078a2c4a53e11742878ef76fa46bb0891
Joe Sweeney <joe@j1.io>
2021-04-14 13:06:58 -0400

    Rename messages migration, reference members

--- priv/repo/migrations/20210414032314_add_messages_table.exs
+++ priv/repo/migrations/20210414032314_add_messages_table.exs
@@ -4,7 +4,8 @@ defmodule Edgybot.Repo.Migrations.AddMessagesTable do
   def change do
     create table("messages", primary_key: false) do
       add :id, :bigint, primary_key: true
-      add :user_id, references(:users, type: :bigint)
+      add :guild_id, :bigint, null: false
+      add :user_id, references(:members, column: :user_id, with: [guild_id: :guild_id], name: :messages_member_fkey, type: :bigint, match: :full), null: false
 
       timestamps()
     end
