14cabafff150b580ff0d0ceb610e53a0dd5bb2e0
Joe Sweeney <joe@j1.io>
2021-05-08 16:40:17 -0400

    Associate messages with channels (#28)
    * Associate messages with channels in migration
    
    * Associate messages with channels in model
    
    * Add tests for messages associated with channels

--- lib/edgybot/meta/message.ex
+++ lib/edgybot/meta/message.ex
@@ -4,21 +4,23 @@ defmodule Edgybot.Meta.Message do
   use Ecto.Schema
   import Ecto.Changeset
   import Edgybot.Meta.Snowflake
-  alias Edgybot.Meta.User
+  alias Edgybot.Meta.{Channel, User}
 
   @primary_key {:id, :id, autogenerate: false}
   schema "messages" do
     belongs_to(:user, User)
+    belongs_to(:channel, Channel)
 
     timestamps()
   end
 
   def changeset(message, params \\ %{}) do
     message
-    |> cast(params, [:id, :user_id])
-    |> validate_required([:id, :user_id])
+    |> cast(params, [:id, :user_id, :channel_id])
+    |> validate_required([:id, :user_id, :channel_id])
     |> validate_snowflake(:id)
     |> assoc_constraint(:user)
+    |> assoc_constraint(:channel)
     |> unique_constraint(:id, name: :messages_pkey)
   end
 end
