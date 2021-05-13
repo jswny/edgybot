dd8d7b5fd1a28636444a3d1a859191a490ad5f43
Joe Sweeney <joe@j1.io>
2021-05-13 18:52:30 -0400

    Add roles migration

---
+++ priv/repo/migrations/20210513222149_add_roles_table.exs
@@ -0,0 +1,12 @@
+defmodule Edgybot.Repo.Migrations.AddRolesTable do
+  use Ecto.Migration
+
+  def change do
+    create table("roles", primary_key: false) do
+      add :id, :bigint, primary_key: true
+      add :guild_id, references(:guilds, type: :bigint)
+
+      timestamps()
+    end
+  end
+end
