9ce8dffd36637e35b25c62d857168d7cc5600560
Joe Sweeney <joe@j1.io>
2021-07-10 14:58:25 -0400

    Add eval command

--- lib/edgybot/bot/command/commands/dev.ex
+++ lib/edgybot/bot/command/commands/dev.ex
@@ -13,6 +13,19 @@ defmodule Edgybot.Bot.Command.Dev do
           name: "error",
           description: "Purposefully error handling a command",
           type: 1
+        },
+        %{
+          name: "eval",
+          description: "Evaluate some Elixir code",
+          type: 1,
+          options: [
+            %{
+              name: "code",
+              description: "The code to be evaluated",
+              type: 3,
+              required: true
+            }
+          ]
         }
       ]
     }
@@ -20,17 +33,34 @@ defmodule Edgybot.Bot.Command.Dev do
 
   @impl true
   def handle_interaction(interaction) do
-    subcommand_name =
+    subcommand_option =
       interaction
       |> Map.get(:data)
       |> Map.get(:options)
       |> Enum.at(0)
-      |> Map.get(:name)
+
+    subcommand_name = Map.get(subcommand_option, :name)
 
     case subcommand_name do
-      "error" -> _ = raise("fake error")
-    end
+      "error" ->
+        _ = raise("fake error")
 
-    {:error, "Unhandled subcommand"}
+      "eval" ->
+        handle_subcommand_eval(subcommand_option)
+
+      _ ->
+        {:error, "Unhandled subcommand"}
+    end
+  end
+
+  def handle_subcommand_eval(subcommand_option) do
+    code_string =
+      subcommand_option
+      |> Map.get(:options)
+      |> Enum.at(0)
+      |> Map.get(:value)
+
+    {result, _binding} = Code.eval_string(code_string)
+    {:message, "`#{result}`"}
   end
 end
