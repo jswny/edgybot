24f0f31e23305ca77f7927989e43bc6b936085c2
Joe Sweeney <joe@j1.io>
2021-07-11 13:08:40 -0400

    Use default application ID for registering commands

--- lib/edgybot/bot/handler/guild.ex
+++ lib/edgybot/bot/handler/guild.ex
@@ -4,18 +4,16 @@ defmodule Edgybot.Bot.Handler.Guild do
   require Logger
   alias Edgybot.Bot.Command.Registrar
   alias Nostrum.Api
-  alias Nostrum.Cache.Me
 
   def handle_guild_available(guild) when is_map(guild) do
-    application_id = get_discord_application_id()
     guild_id = guild.id
     commands = Registrar.list_commands()
 
-    register_guild_commands(application_id, guild_id, commands)
+    register_guild_commands(guild_id, commands)
   end
 
-  defp register_guild_commands(application_id, guild_id, commands)
-       when is_integer(application_id) and is_integer(guild_id) and is_list(commands) do
+  defp register_guild_commands(guild_id, commands)
+       when is_integer(guild_id) and is_list(commands) do
     Logger.info("Registering commands for guild #{guild_id}...")
 
     commands
@@ -23,15 +21,11 @@ defmodule Edgybot.Bot.Handler.Guild do
       Task.async(fn ->
         command_name = command.name
         Logger.info("Registering command #{command_name} for guild #{guild_id}...")
-        Api.create_guild_application_command(application_id, guild_id, command)
+        Api.create_guild_application_command(guild_id, command)
       end)
     end)
     |> Task.await_many()
 
     :noop
-  end
-
-  defp get_discord_application_id do
-    Me.get().id
   end
 end
