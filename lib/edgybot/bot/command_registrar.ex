52e9d0beff68029a56d80855ea18154d6ae989b5
Joe Sweeney <joe@j1.io>
2021-07-19 10:48:26 -0400

    Refactor command registrar into GenServer

--- lib/edgybot/bot/command_registrar.ex
+++ lib/edgybot/bot/command_registrar.ex
@@ -1,6 +1,7 @@
 defmodule Edgybot.Bot.CommandRegistrar do
   @moduledoc false
 
+  use GenServer
   alias Edgybot.Bot.Command.{Dev, Ping}
 
   @command_modules [
@@ -8,11 +9,36 @@ defmodule Edgybot.Bot.CommandRegistrar do
     Dev
   ]
 
-  def get_command_module(name) do
-    Enum.find(@command_modules, fn module -> module.get_command().name == name end)
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def get_command_module(command_name) do
+    GenServer.call(__MODULE__, {:get_module, command_name})
   end
 
   def list_commands do
-    Enum.map(@command_modules, fn module -> module.get_command() end)
+    GenServer.call(__MODULE__, :list_commands)
+  end
+
+  @impl true
+  def init(_opts) do
+    state = %{command_modules: @command_modules}
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:get_module, command_name}, _from, %{command_modules: command_modules} = state) do
+    module =
+      Enum.find(command_modules, fn module -> module.get_command().name == command_name end)
+
+    {:reply, module, state}
+  end
+
+  @impl true
+  def handle_call(:list_commands, _from, %{command_modules: command_modules} = state) do
+    commands = Enum.map(command_modules, fn module -> module.get_command() end)
+
+    {:reply, commands, state}
   end
 end
