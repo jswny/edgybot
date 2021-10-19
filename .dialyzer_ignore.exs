4643c0580d579d177a14dbb191954fde155e5327
Joe Sweeney <joe@j1.io>
2021-10-19 16:05:47 -0400

    Add ability to clear nicknames (#50)
    * Add ability to clear nicknames
    
    * More robust Dialyzer ignores for nickname command

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,4 @@
 [
   ~r/.*nick.ex:\d+:pattern_match The pattern can never match the type/,
-  ~r/.*nick.ex:\d+:unused_fun Function handle_max_length_error\/4 will never be called/
+  ~r/.*nick.ex:\d+:unused_fun Function handle_max_length_error.* will never be called/
 ]
