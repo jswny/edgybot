9e1cece584e8cfdcd08397d331ae289a2881352f
Joe Sweeney <joe@j1.io>
2021-11-12 11:15:12 -0500

    Add ID to reactions, switch emoji string to emote ID integer

--- lib/edgybot/meta/reaction.ex
+++ lib/edgybot/meta/reaction.ex
@@ -3,23 +3,25 @@ defmodule Edgybot.Meta.Reaction do
 
   use Ecto.Schema
   import Ecto.Changeset
+  import Edgybot.Meta.Snowflake
   alias Edgybot.Meta.{Member, Message}
 
-  @primary_key {:id, :id, autogenerate: false}
   schema "reactions" do
     belongs_to(:message, Message)
     belongs_to(:member, Member)
-    field(:emoji, :string)
+
+    field(:emote_id, :id)
 
     timestamps()
   end
 
   def changeset(reaction, params \\ %{}) do
     reaction
-    |> cast(params, [:message_id, :member_id, :emoji])
-    |> validate_required([:message_id, :member_id, :emoji])
+    |> cast(params, [:message_id, :member_id, :emote_id])
+    |> validate_required([:message_id, :member_id, :emote_id])
+    |> validate_snowflake(:emote_id)
     |> assoc_constraint(:message)
     |> assoc_constraint(:member)
-    |> unique_constraint([:message_id, :member_id, :emoji])
+    |> unique_constraint([:message_id, :member_id, :emote_id])
   end
 end
