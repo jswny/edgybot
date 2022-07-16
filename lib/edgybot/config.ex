965f110ad9bfd9fb381b4de885425254a07eebe6
Joe Sweeney <joe@j1.io>
2022-07-16 16:33:58 -0400

    Add meme command

--- lib/edgybot/config.ex
+++ lib/edgybot/config.ex
@@ -3,6 +3,8 @@ defmodule Edgybot.Config do
 
   def runtime_env, do: fetch(:runtime_env)
 
+  def memegen_url, do: fetch(:memegen_url)
+
   defp fetch(key) when is_atom(key) do
     __MODULE__
     |> Application.get_application()
