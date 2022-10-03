6fc065d0ff4ee6e9c0fbdf78c27636b63920e9bb
Joe Sweeney <joe@j1.io>
2022-10-03 00:19:09 -0400

    Add slowmode command (#77)

---
+++ lib/edgybot/bot/plugin/slow_plugin.ex
@@ -0,0 +1,69 @@
+defmodule Edgybot.Bot.Plugin.SlowPlugin do
+  @moduledoc false
+
+  alias Edgybot.Bot.Designer
+  alias Nostrum.Api
+
+  @behaviour Edgybot.Bot.Plugin
+
+  @impl true
+  def get_plugin_definitions do
+    [
+      %{
+        application_command: %{
+          name: "slow",
+          description: "Modify slowmode for the current channel",
+          type: 1,
+          options: [
+            %{
+              name: "enable",
+              description: "Turn slowmode on, or adjust the current slowmode",
+              type: 1,
+              options: [
+                %{
+                  name: "delay",
+                  description: "Length (in seconds) users have to wait between messages",
+                  type: 4,
+                  required: true
+                }
+              ]
+            },
+            %{
+              name: "disable",
+              description: "Turn slowmode off",
+              type: 1
+            }
+          ]
+        }
+      }
+    ]
+  end
+
+  @impl true
+  def handle_interaction(
+        ["slow", "enable"],
+        1,
+        [{"delay", 4, delay}],
+        %{channel_id: channel_id},
+        _middleware_data
+      ) do
+    lower_bound = 1
+    upper_bound = 21_600
+
+    if delay < lower_bound or delay > upper_bound do
+      {:warning,
+       "Delay must be between #{Designer.code_inline(Integer.to_string(lower_bound))} and #{Designer.code_inline(Integer.to_string(upper_bound))}!"}
+    else
+      Api.modify_channel!(channel_id, %{rate_limit_per_user: delay})
+
+      {:success,
+       "Sucessfully enabled slow mode with a delay of #{Designer.code_inline(Integer.to_string(delay))}!"}
+    end
+  end
+
+  @impl true
+  def handle_interaction(["slow", "disable"], 1, [], %{channel_id: channel_id}, _middleware_data) do
+    Api.modify_channel!(channel_id, %{rate_limit_per_user: 0})
+    {:success, "Successfully disabled slowmode!"}
+  end
+end
