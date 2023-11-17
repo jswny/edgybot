5e731e5efa477c1b5131139572f2e637f1147ef5
Joe Sweeney <joe@j1.io>
2023-11-16 23:10:35 -0500

    Use archive.today for archiving (#92)

--- lib/edgybot/bot/plugin/archive_plugin.ex
+++ lib/edgybot/bot/plugin/archive_plugin.ex
@@ -32,29 +32,8 @@ defmodule Edgybot.Bot.Plugin.Archive do
         _interaction,
         _middleware_data
       ) do
-    internet_archive_url = "https://web.archive.org/save/#{url}"
+    archived_url = "https://archive.today/latest/#{url}"
 
-    response_tuple =
-      :get
-      |> Finch.build(internet_archive_url)
-      |> Finch.request(FinchPool, receive_timeout: 840_000)
-
-    case response_tuple do
-      {:ok, response} ->
-        location_header = List.keyfind(response.headers, "location", 0)
-
-        case location_header do
-          {_, archived_url} ->
-            {:message, "**Original link**: #{url}\n\n**Archived at**: <#{archived_url}>"}
-
-          _ ->
-            {:error,
-             "The Internet Archive could not archive that page! It may not be supported for archiving."}
-        end
-
-      {:error, %Mint.TransportError{reason: :timeout}} ->
-        {:warning,
-         "The Internet Archive did not respond in time. Some articles may take a while to archive for the first time. Please try again in the future."}
-    end
+    {:message, "**Original link**: #{url}\n\n**Archived at**: <#{archived_url}>"}
   end
 end
