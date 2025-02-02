56c4dc9b1319cfbea26abcb538dd03b62d128e49
Joe Sweeney <joe@j1.io>
2025-02-01 19:01:21 -0500

    Universal context (#114)
    Inject universal context into chat

--- .credo.exs
+++ .credo.exs
@@ -4,7 +4,8 @@
       name: "default",
       checks: %{
         extra: [
-          {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 15}
+          {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 15},
+          {Credo.Check.Refactor.FunctionArity, max_arity: 10}
         ],
         disabled: [
           {Credo.Check.Readability.ModuleDoc, []}
